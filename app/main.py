04032ea3d56fac4321b6dfd4f473b74576ef5fad
= <=>
2025-01-03 17:00:52 +0100

    add prefix to router

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 from . import crud, database, models, schemas
 
 app = FastAPI()
-router = APIRouter()
+router = APIRouter(prefix="/songs")
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,12 +21,7 @@ def read_root():
     return {"message": "Hello World"}
 
 
-@router.post("/songs", response_model=schemas.SongOut)
-def create_song(song: schemas.SongCreate, db: Session = Depends(database.get_db)):
-    return crud.create_song(db, song)
-
-
-@router.get("/songs/{song_id}", response_model=schemas.SongOut)
+@router.get("/{song_id}", response_model=schemas.SongOut)
 def read_song(song_id: int, db: Session = Depends(database.get_db)):
     song = crud.get_song(db, song_id)
     if not song:
@@ -34,14 +29,19 @@ def read_song(song_id: int, db: Session = Depends(database.get_db)):
     return song
 
 
-@router.get("/songs/", response_model=list[schemas.SongOut])
+@router.get("/", response_model=list[schemas.SongOut])
 def read_songs(
     skip: int = 0, limit: int = 10, db: Session = Depends((database.get_db))
 ):
     return crud.get_songs(db, skip=skip, limit=limit)
 
 
-@router.put("/songs/{song_id}", response_model=schemas.SongOut)
+@router.post("/", response_model=schemas.SongOut)
+def create_song(song: schemas.SongCreate, db: Session = Depends(database.get_db)):
+    return crud.create_song(db, song)
+
+
+@router.put("/{song_id}", response_model=schemas.SongOut)
 def update_song(
     song_id: int, song: schemas.SongUpdate, db: Session = Depends(database.get_db)
 ):
@@ -51,9 +51,13 @@ def update_song(
     return updated_song
 
 
-@router.delete("songs/{song_id}", response_model=schemas.SongOut)
+@router.delete("/{song_id}", response_model=schemas.SongOut)
 def delete_song(song_id: int, db: Session = Depends(database.get_db)):
     deleted_song = crud.delete_song(db, song_id)
     if not deleted_song:
         raise HTTPException(status_code=404, detail="song not found")
     return deleted_song
+
+
+# Include the router AFTER defining the routes
+app.include_router(router)
